b680182a4d852ba0559a2a56f44bbb8c57f8664c
mohd-raza1 <mohd.raza@kartrocket.com>
2020-12-27 07:54:10 +0530

    q3 constants solved

--- exercise_2/q1.go
+++ exercise_2/q1.go
@@ -4,15 +4,13 @@ import (
 	"fmt"
 )
 
+const (
+	a     = 45
+	b int = 56
+)
+
 func main() {
 
-	a := (4 == 5)
-	b := (5 <= 2)
-	c := (15 >= 14)
-	d := (4 != 6)
-	e := (56 > 45)
-	f := (56 < 98)
-
-	fmt.Println(a, b, c, d, e, f)
+	fmt.Println(a, b)
 
 }
